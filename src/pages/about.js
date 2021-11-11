facfd237351f5c479827926ad2b7d42b629b8787
asakusa <albertsson.vincent@gmail.com>
2021-11-11 20:03:52 +0100

    Finished Gatsby Tutorial Part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Layout from '../components/layout';
 
 const AboutPage = () => {
-  return(
+  return (
     <Layout pageTitle="About Page">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
