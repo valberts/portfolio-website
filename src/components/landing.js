0dd2ce5c9f5236f0823dc5ec959e5a4cb02f3bc6
asakusa <albertsson.vincent@gmail.com>
2021-11-19 20:47:20 +0100

    Fixed landing page spacing

--- src/components/landing.js
+++ src/components/landing.js
@@ -15,7 +15,7 @@ const Landing = () => {
       <h4 className={intro}>Hi, my name is...</h4>
       <h1 className={nameHeading}>Vincent Albertsson</h1>
       <h2 className={introduction}>I'm a computer science student currently building up my portfolio.</h2>
-      <h4 className={brief}>I'm coming up with ideas and working on projects to figure out what I like and get some more experience.</h4>
+      <h3 className={brief}>I'm coming up with ideas and working on projects to figure out what I like and get some more experience.</h3>
     </div>
   )
 }
